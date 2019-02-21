5aed713bb51c7150581a404711a694e39b1cbd3f
S Minutoli <sergio.minutoli@gmail.com>
2019-02-21 04:13:13 -0300

    Objetos bien posicionados

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const config = {
 };
 
 function create() {
-  var fondoArriba = 0xFF0000;
-  var fondoAbajo = 0xFFFF00;
+  var fondoArriba = 0x8787FF;
+  var fondoAbajo = 0xFFFFFF;
   var verde = 0x00FF00;
   var azul = 0x0000FF;
   
@@ -17,13 +17,17 @@ function create() {
   fondo.fillGradientStyle(fondoArriba, fondoArriba, fondoAbajo, fondoAbajo, 1, 1);
   fondo.fillRect(0, 0, config.width, config.height);
 
-  var personaje = this.add.rectangle(180, 595, 64, 64, verde);
+  var personaje = this.add.rectangle(255, 595, 64, 64, verde);
 
-  var plataforma = this.add.rectangle(180, 355, 140, 30, azul);
+  var plataforma = this.add.rectangle(290, 355, 140, 30, azul);
 
-  var plataforma2 = this.add.rectangle(180, 131, 134, 25, azul);
+  var plataforma2 = this.add.rectangle(293, 131, 134, 25, azul);
 
-  var plataforma3 = this.add.rectangle(180, 207, 146, 26, azul);
+  var plataforma3 = this.add.rectangle(73, 207, 146, 26, azul);
+
+  var plataforma4 = this.add.rectangle(180, 425, 96, 26, azul);
+
+  var plataforma5 = this.add.rectangle(68, 512, 135, 25, azul);
 
   var piso = this.add.rectangle(182, 640, config.width, 25, azul);
 }
