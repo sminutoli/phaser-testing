5f046f371df267ed8eef040190c836d74c8ed76d
S Minutoli <sergio.minutoli@gmail.com>
2019-02-21 04:26:02 -0300

    Agrega todas las imagenes

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const config = {
   width: 360,
   height: 640,
   scene: {
-    create
-  }
+    create,
+    preload
+  },
+  pixelArt: true,
 };
+
+function preload() {
+  this.load.image('plataforma', 'assets/pisos/plataforma1.png');
+  this.load.image('plataforma2', 'assets/pisos/plataforma2.png');
+  this.load.image('plataforma4', 'assets/pisos/plataforma4.png');
+  this.load.image('piso', 'assets/pisos/piso.png');
+  this.load.image('estrella', 'assets/estrella.png');
+  this.load.image('trofeo', 'assets/trofeo.png');
+  this.load.image('malo', 'assets/malo.png');
+  this.load.image('personaje', 'assets/personaje.png');
+}
 
 function create() {
   var fondoArriba = 0x8787FF;
@@ -17,19 +30,29 @@ function create() {
   fondo.fillGradientStyle(fondoArriba, fondoArriba, fondoAbajo, fondoAbajo, 1, 1);
   fondo.fillRect(0, 0, config.width, config.height);
 
-  var personaje = this.add.rectangle(255, 595, 64, 64, verde);
+  this.add.image(255, 605, 'personaje');
 
-  var plataforma = this.add.rectangle(290, 355, 140, 30, azul);
+  this.add.image(290, 355, 'plataforma');
+  this.add.image(293, 131, 'plataforma2');
+  this.add.image(73, 207,  'plataforma2');
+  this.add.image(180, 425, 'plataforma4');
+  this.add.image(68, 512, 'plataforma2');
 
-  var plataforma2 = this.add.rectangle(293, 131, 134, 25, azul);
+  this.add.image(182, 640, 'piso');
 
-  var plataforma3 = this.add.rectangle(73, 207, 146, 26, azul);
+  this.add.image(300, 80, 'trofeo').setScale(0.5);
 
-  var plataforma4 = this.add.rectangle(180, 425, 96, 26, azul);
+  this.add.image(40, 170, 'estrella');
+  this.add.image(260, 320, 'estrella');
+  this.add.image(310, 320, 'estrella');
+  this.add.image(30, 475, 'estrella');
+  this.add.image(80, 475, 'estrella');
+  this.add.image(80, 605, 'estrella');
+  this.add.image(130, 605, 'estrella');
 
-  var plataforma5 = this.add.rectangle(68, 512, 135, 25, azul);
-
-  var piso = this.add.rectangle(182, 640, config.width, 25, azul);
+  this.add.image(271, 327, 'malo').setScale(2.2);
+  this.add.image(80, 482, 'malo').setScale(2.2);
+  this.add.image(112, 603, 'malo').setScale(2.2);
 }
 
 new Phaser.Game(config);
