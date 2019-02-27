3774c9f3461603048a8fe4decf5d0f381c80fe4e
S Minutoli <sergio.minutoli@gmail.com>
2019-02-27 20:48:15 -0300

    Paso 3: imagenes colocadas con fisica

--- index.js
+++ index.js
@@ -1,58 +1,114 @@
-const config = {
+var config = {
   type: Phaser.AUTO,
   width: 360,
   height: 640,
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 300 },
+          debug: false
+      }
+  },
+  /*
+  Tenemos que conectar las etapas del juego con nuestras funciones!
+  */
   scene: {
-    create,
-    preload
+    preload: cargarAntesDeEmpezar,
+    create: inicializarJuego
   },
   pixelArt: true,
 };
 
-function preload() {
+/********** VARIABLES *************/
+
+var plataformas;
+var trofeo;
+var enemigos;
+var premios;
+var personaje;
+
+/********** FIN DE VARIABLES *************/
+
+
+/*
+La función cargarAntesDeEmpezar() nos permite precargar imágenes / sonidos / textos para que el juego comience sin fallas. La conectamos al juego en la sección de config (arriba)
+*/ 
+function cargarAntesDeEmpezar() {
+  // la primer palabra es el nombre clave de la imagen, la segunda la ubicación del archivo real, no tienen por qué coincidir
   this.load.image('plataforma', 'assets/pisos/plataforma1.png');
   this.load.image('plataforma2', 'assets/pisos/plataforma2.png');
   this.load.image('plataforma4', 'assets/pisos/plataforma4.png');
   this.load.image('piso', 'assets/pisos/piso.png');
-  this.load.image('estrella', 'assets/estrella.png');
+  this.load.image('premio', 'assets/estrella.png');
   this.load.image('trofeo', 'assets/trofeo.png');
-  this.load.image('malo', 'assets/malo.png');
+  this.load.image('enemigo', 'assets/malo.png');
   this.load.image('personaje', 'assets/personaje.png');
 }
 
-function create() {
-  var fondoArriba = 0x8787FF;
-  var fondoAbajo = 0xFFFFFF;
-  var verde = 0x00FF00;
-  var azul = 0x0000FF;
+/*
+La función inicializarJuego() nos permite inicializar los elementos del juego
+*/
+function inicializarJuego() {
+  
+  /********** CREACION DE FONDO *************/
   
   var fondo = this.add.graphics();
-  fondo.fillGradientStyle(fondoArriba, fondoArriba, fondoAbajo, fondoAbajo, 1, 1);
+  fondo.fillGradientStyle(0x8787FF, 0x8787FF, 0xFFFFFF, 0xFFFFFF, 1, 1);
   fondo.fillRect(0, 0, config.width, config.height);
+  
+  /********** FIN CREACION DE FONDO *************/
 
-  this.add.image(255, 605, 'personaje');
+  /********** CREACION DE PLATAFORMAS *************/
+  
+  /*
+  Un staticGroup() contiene elementos que no van a seguir las leyes de la física como la gravedad.
+  */
+  plataformas = this.physics.add.staticGroup(); // probá poniendo group() en vez de staticGroup()!
+  /*
+  Parecido a this.add.image() o this.add.rectangle()
+  Todo staticGroup permite usar su función create() que necesita 3 ingredientes:
+    - x, y, 'nombre clave de la imagen' (como la definimos en preload)
+  */
+  plataformas.create(290, 355, 'plataforma'); 
+  plataformas.create(293, 131, 'plataforma2');
+  plataformas.create(73, 207,  'plataforma2');
+  plataformas.create(180, 425, 'plataforma4');
+  plataformas.create(68, 512, 'plataforma2');
+  plataformas.create(182, 630, 'piso');
+  
+  /********** FIN DE CREACION DE PLATAFORMAS *************/
+  
+  /********** CREACION DE TROFEO *************/
+  
+  trofeo = this.physics.add.image(300, 80, 'trofeo');
+  trofeo.setScale(0.5);
+  this.physics.add.collider(trofeo, plataformas);
+  
+  /********** FIN DE CREACION DE TROFEO *************/
 
-  this.add.image(290, 355, 'plataforma');
-  this.add.image(293, 131, 'plataforma2');
-  this.add.image(73, 207,  'plataforma2');
-  this.add.image(180, 425, 'plataforma4');
-  this.add.image(68, 512, 'plataforma2');
-
-  this.add.image(182, 640, 'piso');
-
-  this.add.image(300, 80, 'trofeo').setScale(0.5);
-
-  this.add.image(40, 170, 'estrella');
-  this.add.image(260, 320, 'estrella');
-  this.add.image(310, 320, 'estrella');
-  this.add.image(30, 475, 'estrella');
-  this.add.image(80, 475, 'estrella');
-  this.add.image(80, 605, 'estrella');
-  this.add.image(130, 605, 'estrella');
-
-  this.add.image(271, 327, 'malo').setScale(2.2);
-  this.add.image(80, 482, 'malo').setScale(2.2);
-  this.add.image(112, 603, 'malo').setScale(2.2);
+  /********** CREACION DE PREMIOS *************/
+  
+  premios = this.physics.add.group();  
+  premios.create(40, 160, 'premio');
+  this.physics.add.collider(premios, plataformas);
+  
+  /********** FIN DE CREACION DE PREMIOS *************/
+  
+  /********** CREACION DE ENEMIGOS *************/
+  
+  enemigos = this.physics.add.group(); 
+  enemigos.create(271, 327, 'enemigo').setScale(2.2);
+  this.physics.add.collider(enemigos, plataformas);
+  
+  /********** FIN DE CREACION DE ENEMIGOS *************/
+  
+  /********** CREACION DE PERSONAJE *************/
+  
+  personaje = this.physics.add.image(255, 500, 'personaje');
+  this.physics.add.collider(personaje, plataformas);
+  
+  /********** FIN DE CREACION DE PERSONAJE *************/
+  
 }
 
 new Phaser.Game(config);
